f691842c8cf81e2a7c442e4491ae1f0e6c93f496
Keelan Brening <kbrening@odin.cs.csubak.edu>
2021-02-28 20:07:13 -0800

    Working on Login System, still some bugs

--- login.php
+++ login.php
@@ -1,9 +1,83 @@
+<!doctype html>
 <html>
     <head>
-        <title>Quiz-Taker Login</title>
+        <title>Login</title>
     </head>
     <body>
-        <?php include('nav.php'); ?>
-        <h1>login page</h1>
+<?php session_start(); ?>
+    <div id='nav'>
+        <?php include('nav.php');?>
+    </div>
+<?php
+//session_start();
+//$server = 'localhost';
+//$user = 'kbrening';
+//$password = '1234';
+//$database = 'kbrening';
+//$db = new mysqli($server, $user, $password, $database);
+
+include('config.php');
+
+if(isset($_SESSION['userlogged']) && $_SESSION['userlogged'] == true) {
+    //redirect to home.php. nop need for login when you are logged i
+    header("Location: home.php");
+} else if(isset($_SESSION['adminlogged']) && $_SESSION['adminlogged'] == true) {
+    header("Location: home.php");
+} else {
+    if(isset($_POST['fname']) && isset($_POST['pass']) && !empty($_POST['fname']) && !empty($_POST['pass'])){
+
+        $fname = htmlentities(strip_tags($_POST['fname']), ENT_QUOTES);
+        $tpass = htmlentities(strip_tags($_POST['pass']), ENT_QUOTES);
+        $pass = password_hash($tpass, PASSWORD_BCRYPT);
+
+        if($db->connect_error){
+            exit("Bad Connection");
+        } else {
+            $res = $db -> query("SELECT * FROM Student WHERE C_fname = '{$fname}'");
+
+            if($res)
+                if($row = $res->fetch_assoc()){
+                    
+                    $isfnameValid = ($fname == $row['C_fname']);
+                    $ispassValid = ($pass == $row['C_password']);
+                    
+                    if($isfnameValid == true && $ispassValid == true) {
+                        $_SESSION['userlogged'] = true;
+                        $_SESSION['fname'] = $row['C_fname'];
+                        $_SESSION['id'] = $row['C_id'];
+                        header("Location: home.php");
+                        //echo "correct info";
+                    //need to check for admin/Teacher
+                    /*} else if($isemailValid == true && $ispassValid == true ) {
+                        $_SESSION['userlogged'] = true;
+                        $_SESSION['fname'] = $row['user_fname'];
+                        $_SESSION['email'] = $row['user_email'];
+                        header("Location: home.php");
+                    */
+                    } else {
+                        header("Location: login.php");
+                    }
+
+                }
+        }
+
+
+    } else {
+?>
+    
+    <div class=border>
+        <form action='login.php' method='post'>
+            <label for='fname'>First Name</label><br>
+            <input type='text' value='' name='fname'><br>
+            <label for='pass'>Password</label><br>
+            <input type='password' value='' name='pass'>
+            <br><input type='submit' value='Submit'>
+        </form>
+    </div>
+    <!--<br><a href='createlogin.php'>Create Login</a>";-->
+<?php
+    }
+}
+?>
     </body>
 </html>
--- nav.php
+++ nav.php
@@ -1,22 +1,65 @@
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-    </head>
-    <body>
-        <nav class="navbar navbar-expand-sm bg-secondary navbar-dark">
-            <ul class="navbar-nav">
-                <li class="nav-item active">
-                    <a class="nav-link" href="home.php">Home</a>
-                </li>
-                <li class="nav-item active">
-                    <a class="nav-link" href="login.php">Login</a>
-                </li>
-            </ul>
-        </nav>
-    </body>
-</html>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+<?php
+    if(isset($_SESSION['userlogged']) && $_SESSION['userlogged'] == true) {
+
+        echo "<nav class='navbar navbar-expand-sm bg-secondary navbar-dark'>";
+        echo "<ul class='navbar-nav'>";
+        echo "<li class='nav-item active'>";
+        echo "<a class='nav-link' href='home.php'>Home</a>";
+        echo "</li>";
+        echo "<li class='nav-item active'>";
+        echo "<a class='nav-link' href='*'>Take Quiz</a>";
+        echo "</li>";
+        echo "<li class='nav-item active'>";
+        echo "<a class='nav-link' href='*'>Scores</a>";
+        echo "</li>";
+        echo "<li class='nav-item active'>";
+        echo "<a class='nav-link' href='logout.php'>Logout</a>";
+        echo "</li>";
+        echo "</ul>";
+        echo "</nav>";
+
+    } else if(isset($_SESSION['adminlogged']) && $_SESSION['adminlogged'] == true) {
+
+        echo "<nav class='navbar navbar-expand-sm bg-secondary navbar-dark'>";
+        echo "<ul class='navbar-nav'>";
+        echo "<li class='nav-item active'>";
+        echo "<a class='nav-link' href='home.php'>Home</a>";
+        echo "</li>";
+        echo "<li class='nav-item active'>";
+        echo "<a class='nav-link' href='*'>Create User</a>";
+        echo "</li>";
+        echo "<li class='nav-item active'>";
+        echo "<a class='nav-link' href='*'>Create Quiz</a>";
+        echo "</li>";
+        echo "<li class='nav-item active'>";
+        echo "<a class='nav-link' href='*'>View all Scores</a>";
+        echo "</li>";
+        echo "<li class='nav-item active'>";
+        echo "<a class='nav-link' href='logout.php'>Logout</a>";
+        echo "</li>";
+        echo "</ul>";
+        echo "</nav>";
+
+
+    } else {
+
+        echo "<nav class='navbar navbar-expand-sm bg-secondary navbar-dark'>";
+        echo "<ul class='navbar-nav'>";
+        echo "<li class='nav-item active'>";
+        echo "<a class='nav-link' href='home.php'>Home</a>";
+        echo "</li>";
+        echo "<li class='nav-item active'>";
+        echo "<a class='nav-link' href='login.php'>Login</a>";
+        echo "</li>";
+        echo "</ul>";
+        echo "</nav>";
+
+
+    }
+?>
